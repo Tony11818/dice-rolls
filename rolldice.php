9317a8c149a95731afed9fdd9f30dbc84720f4dd
Aurelio Anselmo <anthonyanselmo92@gmail.com>
2019-09-28 20:04:33 -0700

    testing out things

--- rolldice.php
+++ rolldice.php
@@ -44,15 +44,13 @@
 
   if ($_POST[submitted])
     {
-        echo "Process form";
+        echo $firstRoll;
 
     }
     else
     {
     echo <<< _ENDOFFORM_
       <form method="post" action="$_SERVER[SCRIPT_NAME]">
-      <label for="fname">First Name</label>
-      <input type="text" name="first_name" id="fname" /><br />
       <input type="submit" name="submitted" />
       </form>
 _ENDOFFORM_;

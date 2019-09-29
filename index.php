ac93b70e86ef38fbe2be5953840ffce88fb58d5a
Aurelio Anselmo <anthonyanselmo92@gmail.com>
2019-09-28 18:51:30 -0700

    testing out things

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <title>Roll My Dice</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1"/>
     <meta name="dcterms.rightsHolder" content="Aurelio Anselmo" />
     <meta name="dcterms.dateCopyrighted" content="2019" />
     <meta name="description" content="Roll Dice Game" />

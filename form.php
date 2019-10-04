0cbfc757eafb54a6d1a1f91a20913239a311f139
Aurelio Anselmo <anthonyanselmo92@gmail.com>
2019-10-03 17:33:11 -0700

    testing out things

--- form.php
+++ form.php
@@ -5,6 +5,7 @@
       if (!isset($_POST['name']))
       {
         killsession($yourpath);
+        unset($totalRolls);
     ?>
       <label for="name">Players Name:</label>
       <br/>
@@ -19,10 +20,9 @@
             echo '<input type="hidden" id="name" name="name" value='.$_SESSION['name'].'>';
             if(!isset($_SESSION['name'])){
                 $_SESSION['name'] = $_POST['name'];
-                unset($totalRolls);
                 echo "<p>What's up ".$_POST['name']."! Roll 10 times and try to get your highest score!</p>";
             } else {
-                echo "<p>Back again, ".$_SESSION['name']."? Want to roll again?</p>";
+                echo "<p>Keep rolling ".$_SESSION['name']."!</p>";
             }
         }
     ?>

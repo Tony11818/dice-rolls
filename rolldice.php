9a0ec3b969d65ab2dfa2acc49bd405feb22e3001
Aurelio Anselmo <anthonyanselmo92@gmail.com>
2019-10-06 14:14:58 -0700

    testing out things

--- rolldice.php
+++ rolldice.php
@@ -1,10 +1,22 @@
 <?php
-   //This is a comment
+// STARTING SESSION COOKIES CODE
   declare(strict_types=1);
+  $yourpath = dirname($_SERVER['SCRIPT_NAME']). '/';
+  $lifetime = 0;  //0 expires when browser closes, otherwise time in seconds
+  session_set_cookie_params ($lifetime,$yourpath);
+  /* Trying to set your own domain, secure and httponly seems to be blocked
+     in modern browsers, and is assumed on if it can.
+     Some Diagnostics you could run if you wanted:
+      echo "<b>Diagnostic Information</b><br />\n";
 
-  // STARTING SESSION COOKIES CODE
-  $yourpath = dirname($_SERVER['SCRIPT_NAME']). '/'; $sessionoptions = [ 'lifetime' => 0, 'path'=> $yourpath,'secure' => TRUE, 'httponly' => TRUE];
-  session_set_cookie_params ($sessionoptions);
+      echo "Session Name: " . session_name() . "<br />\n";
+      echo "Session ID: "  . session_id() . "<br />\n";
+      echo "Session Module name: " . session_module_name() . "<br />\n";
+      echo "Session Save Path: " . session_save_path()  . "<br />\n";
+      echo "Encoded Session: " . session_encode() . "<br />\n";
+      print_r(session_get_cookie_params();
+
+  */
   session_start();
   session_regenerate_id();
 
@@ -13,6 +25,7 @@
     session_unset();
     session_destroy();
     setcookie(session_name(),"",time() - 3600, $cookiepath);
+
   }
   // ENDING SESSION COOKIES CODE
   $diceSideImg = array('dice01','dice02','dice03','dice04','dice05','dice06');

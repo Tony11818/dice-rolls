b5cdc508f12d22d5a0502a26b0fa4f004e579988
Aurelio Anselmo <anthonyanselmo92@gmail.com>
2019-10-07 21:36:25 -0700

    testing out things

--- form.php
+++ form.php
@@ -27,14 +27,18 @@
             echo '<input type="hidden" id="name" name="name" value=' . $_SESSION['name'] . '>';
             if(!isset($_SESSION['name'])){
                 $_SESSION['name'] = $_POST['name'];
-                echo "<p>What's up " . $_POST['name'] . "! Roll 10 times and try to get your highest score!</p>";
-                echo "<p>" . $firstRoll . ">";
+                echo "<p>What's up " . $_POST['name'] . "! Roll 10 times and try to get your highest score!"</p>
             } else {
                 echo "<p>Keep rolling ".$_SESSION['name']."!</p>";
-                echo "<p>" . $totalRolls . ">";
             }
-        }
+          }
     ?>
+<br/>
+<input
+    name="rollDice"
+    value="Roll Dice"
+    type="submit"
+/>
 </form>
   </body>
 </html>
